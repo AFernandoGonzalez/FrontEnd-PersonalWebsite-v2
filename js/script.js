15a9867171da13b2a8315f83e1843b16c7a11783
Fernando <ferg.developer@gmail.com>
2021-12-24 18:18:16 -0500

    Home Page Structure done

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Portfolio Section
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) =>{
+    panel.addEventListener('mouseenter', () =>{
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+
+function removeActiveClasses(){
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
+
+// End Portfolio Section
